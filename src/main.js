da686591420f98c197373ba3e2f9c40d1fdb0653
Philip <philipmjobe@gmail.com>
2021-11-24 18:51:02 -0500

    added apex charts

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import axios from "axios";
+import ApexCharts from "apexcharts";
+
+axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/";
 
 Vue.config.productionTip = false;
 
@@ -8,3 +12,22 @@ new Vue({
   router,
   render: (h) => h(App),
 }).$mount("#app");
+
+var options = {
+  chart: {
+    type: "line",
+  },
+  series: [
+    {
+      name: "sales",
+      data: [30, 40, 35, 50, 49, 60, 70, 91, 125],
+    },
+  ],
+  xaxis: {
+    categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999],
+  },
+};
+
+var chart = new ApexCharts(document.querySelector("#chart"), options);
+
+chart.render();
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,10 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div id="chart"></div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
-export default {
-  name: "Home",
-  components: {
-    HelloWorld,
-  },
-};
 </script>
